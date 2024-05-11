f591c13610cf27700d6e0dfc4c119b1df73e37ab
Hamza Zafar <hamzafer3@gmail.com>
2024-05-11 13:07:28 +0200

    SVM maps done

---
+++ svmmaps.R
@@ -0,0 +1,63 @@
+# Step 1: Load libraries
+library(raster)
+library(dplyr)
+library(mlr)
+library(kernlab)  # using kernlab for SVM
+library(ggplot2)
+library(clue)
+
+# Defining the seed
+set.seed(123, "L'Ecuyer")
+
+# Load the shapefile with training areas
+ROIS <- shapefile("ROIS.shp")
+plot(ROIS)
+
+# Loading the CSV with the training areas
+training <- read.table("training.csv", header = TRUE, sep = ",")
+
+# Removing the column with row IDs
+training <- dplyr::select(training, -X)
+
+# Incorporating the class labels from the vectorial file into the training data frame
+training <- cbind(training, Class_Id = ROIS$Class_Id)
+
+# Converting the class labels to factors as required by mlr and kernlab
+training$Class_Id <- as.factor(training$Class_Id)
+
+# Writing the new data frame with labels
+write.csv(training, file = "training_svm.csv")
+
+# Creating the task for mlr (using factor Class_Id)
+classification.task <- makeClassifTask(id = "nieves", data = training, target = "Class_Id")
+
+# Training a Support Vector Machine model
+svm_model <- ksvm(Class_Id ~ ., data = training, kernel = "rbfdot", C = 1, prob.model = TRUE, kpar = "automatic")
+
+# Printing on screen model information
+print(svm_model)
+
+# Opening the multiband tif
+multiseasonal <- brick("multiseasonal.tif")
+
+# Renaming the bands
+names(multiseasonal) <- c("BlueV", "GreenV", "RedV", "RedEdge1V", "RedEdge2V",
+                          "RedEdge3V", "NIRV", "NIR2V", "SWIR1V", "SWIR2V",
+                          "BlueP", "GreenP", "RedP", "RedEdge1P", "RedEdge2P",
+                          "RedEdge3P", "NIRP", "NIR2P", "SWIR1P", "SWIR2P",
+                          "BlueO", "GreenO", "RedO", "RedEdge1O", "RedEdge2O",
+                          "RedEdge3O", "NIRO", "NIR2O", "SWIR1O", "SWIR2O")
+
+# Applying the model to predict the map
+new_data <- as.data.frame(as.matrix(multiseasonal))
+pred_svm <- predict(svm_model, new_data, type = "response")  # Get class predictions
+map.svm <- multiseasonal[[1]]  # Using the first layer as a template for dimensions
+map.svm[] <- as.numeric(pred_svm)  # Assign predicted values
+map.svm
+
+# Saving models
+save(svm_model, file = "Results/SVM_model.RData")
+
+# Saving the map in tif format
+writeRaster(map.svm, filename = "Results/SVM.tif",
+            format = "GTiff", datatype = "FLT4S", overwrite = TRUE)
